fc2b9700aac455a6d926e6a27e85e4f7dce20204
山本 渚生 <shotayamada7@gmail.com>
2021-09-18 13:06:17 +0900

    ファイル修正

--- work/boardproject/boardapp/views.py
+++ work/boardproject/boardapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+def signupfunc(request):
+    object_list = User.objects.all()
+    # object_list = TodoModel.objects.all()
+    if request.method == 'POST':
+        print('this is post method')
+    else:
+        print('this is not post method')
+    return render(request, 'signup.html', {})
