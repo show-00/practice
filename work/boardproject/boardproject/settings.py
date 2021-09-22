59cb5b84803e41617a37ba98d265acb811c7b68a
山本 渚生 <shotayamada7@gmail.com>
2021-09-22 19:42:33 +0900

    完成　コミット

--- work/boardproject/boardproject/settings.py
+++ work/boardproject/boardproject/settings.py
@@ -119,7 +119,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/sta/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [str(BASE_DIR / 'static')]
+
+MEDIA_URL = 'medi/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
+LOGIN_URL = 'login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
